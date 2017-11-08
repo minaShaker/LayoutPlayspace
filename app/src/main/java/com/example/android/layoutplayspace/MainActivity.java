e76ac5c9500c0d85cb69355e711a3859789a8985
mina shaker <minashaker64@yahoo.com>
2017-11-08 03:26:51 +0200

    add new button and aligning it with the previous one

--- app/src/main/java/com/example/android/layoutplayspace/MainActivity.java
+++ app/src/main/java/com/example/android/layoutplayspace/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.layoutplayspace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*TODO add buttons to change the image*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
